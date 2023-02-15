1dbd9a8d8cba3b1c6578328afa3ae263b98ad214
Brett Cantrell <cantrell.brett8892@gmail.com>
2023-02-15 13:56:49 -0600

    Created Local Setting File

--- products_project/settings.py
+++ products_project/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(c8-)9im130n8pts-l0o257g#srm2nth-47x#2s2xe0)($qmb#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -69,18 +67,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'products_project.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
@@ -121,3 +107,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from products_project.local_settings import *
+except ImportError:
+    pass
